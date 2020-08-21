ecd348ce046e9611e0eb251df607adaa6b903e12
AnahiSamanthaAgredaCruz <a201911877@estudiantes.upsa.edu.bo>
2020-08-21 19:25:33 -0400

    Se ajusto el codigo eliminando (pesos 00/100) y se creo el UnitTest

---
+++ Numero2Letra/UnitTestNumero2Letra/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestNumero2Letra
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
